4ae3ab3d095699e111e0d4ea027eba9f2ea002df
Dan C <djc5627@gmail.com>
2022-05-26 14:42:17 -0400

    Change heading text on click

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -8,8 +8,9 @@ function myFunction() {
   var y = document.getElementById('birds-of-prey');
   if (x.style.display === "none") {
     x.style.display = "block";
-	y.textContent += '+';
+	y.textContent = 'Birds of Prey [-]';
   } else {
     x.style.display = "none";
+	y.textContent = 'Birds of Prey [+]';
   }
 }
